91fa6d337ff2445666522073e82241ea55f3ef3b
sksum <sakshammrigdgr8@gmail.com>
2020-10-04 20:51:22 +0530

    init mf

---
+++ src/Editor.svelte
@@ -0,0 +1,41 @@
+<script>
+    	import { onMount } from 'svelte';
+
+    import {CodeJar} from 'codejar/codejar.js'
+    const highlight = () => {
+    const code = editor.textContent
+    // Do something with code and set html.
+    editor.innerHTML = code
+    }
+
+    onMount(() => {
+        let jar = CodeJar(document.getElementById('editor'),highlight)
+    });
+</script>
+<div id= 'editor'>
+    const hello = "new one ";
+</div >
+
+<style>
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+    #editor {
+        border-radius: 6px;
+        box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+        font-family: 'Source Code Pro', monospace;
+        font-size: 14px;
+        font-weight: 400;
+        height: 340px;
+        width:48%;
+        letter-spacing: normal;
+        line-height: 20px;
+        padding: 10px;
+        tab-size: 4;
+        float:left;
+    }
+</style>
---
+++ src/Toolbar.svelte
@@ -0,0 +1,31 @@
+<script>
+</script>
+
+<label for="userID">userID</label>
+<input type="number" id="userID" name="userID" min="0"> 
+<form action="/action_page.php">
+    <label for="language">Choose a Language:</label>
+    <select id="language" name="language">
+      <option value="cc">C++</option>
+      <option value="c">C</option>
+      <option value="js">Javascript</option>
+      <option value="py">Python</option>
+    </select>
+    <input type="submit" value="Save">
+  </form>
+
+<style>
+
+	label {
+		color: #186896;
+		text-transform: uppercase;
+		font-size: 1em;
+		font-weight: 20;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
