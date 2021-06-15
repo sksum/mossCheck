13f1d1bd0c30a50fc940f7b44b7581595bb5de6c
sksum <sakshammrigdgr8@gmail.com>
2021-06-15 18:30:28 +0530

    port changed

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def hello1():
 
 if __name__ == "__main__":
     app.run(debug =True);
-    # port = int(os.environ.get("PORT", 33507))
-    # app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 33507))
+    app.run(host='0.0.0.0', port=port)
